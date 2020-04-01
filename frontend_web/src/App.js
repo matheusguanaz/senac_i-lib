e8e9c878cc82f68b913055a95080f3139bbeaf81
Diego Capassi <diego.capassi.moreira@gmail.com>
2020-03-31 23:45:29 -0300

    Created the basic structure for the login page

--- frontend_web/src/App.js
+++ frontend_web/src/App.js
@@ -7,14 +7,17 @@ function App() {
     <div className="App">
       <h1>Página de Administração</h1>
       <img src={logo} alt="logo" />`
-      <p>Faça empréstimos de livros e reserve salas de maneira descomplicada</p>
-      <p>Usuário</p>
-      <input type="text" id="loginUsuario"></input>
-      <p>Senha</p>
-      <input type="text" id="loginSenha"></input>
-      <input type="button" id="loginBotao" value="Acessar"></input>
-      <a href="#">Esqueci minha senha</a>
-      <a href="#">Não sou cadastrado</a>
+      <p>
+        Faça empréstimos de livros e reserve <br></br> salas de maneira
+        descomplicada
+      </p>
+      <form>
+        <input type="text" placeholder="Usuário" id="loginUsuario"></input>
+        <input type="text" placeholder="Senha" id="loginSenha"></input>
+        <input type="submit" id="loginBotao" value="Acessar"></input>
+        <a href="#">Esqueci minha senha</a>
+        <a href="#">Não sou cadastrado</a>
+      </form>
     </div>
   );
 }
